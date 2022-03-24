00c19d64f02a9a5c7b5eac1ed51592fab853408c
Denis Budaragin <DBudaragin@more.tv>
2022-03-24 10:27:33 +0300

    [upd] Ex4: GET-запрос

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import requests
+
+# Ex3: Создание репозитория
 print('Hello from Denis')
+
+#Ex4: GET-запрос
+response = requests.get('https://playground.learnqa.ru/api/get_text')
+print(response.text)
+
