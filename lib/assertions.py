146ccad3090a0ee424e3a308c84de1152ef9539f
Denis Budaragin <DBudaragin@more.tv>
2022-04-19 16:09:04 +0300

    [upd] отрефакторил Ассерт для валидации схемы ответа, перенёс к остальным Ассертам в assertions.py

--- lib/assertions.py
+++ lib/assertions.py
@@ -1,6 +1,24 @@
 from requests import Response
 import json
 import deepdiff
+from jsonschema import validate
+from os.path import join, dirname
+
+def assert_valid_schema(data, schema_file_name, schema_file_path):
+    """ Checks whether the given data matches the schema """
+
+    schema = _load_json_schema(schema_file_name, schema_file_path)
+    return validate(data, schema)
+
+def _load_json_schema(filename, file_path):
+    """ Loads the given schema file """
+
+    #relative_path = join('schemas', filename)
+    relative_path = join(file_path, filename)
+    absolute_path = join(dirname(__file__), relative_path)
+
+    with open(absolute_path) as schema_file:
+        return json.loads(schema_file.read())
 
 class Assertions:
     @staticmethod
--- tests/schema_validation/test_schema_validation_for_get_sliders.py
+++ tests/schema_validation/test_schema_validation_for_get_sliders.py
@@ -1,10 +1,9 @@
 import requests
 from resources.models_by_schemas.output_model_for_first_slider import Model
 from pydantic import ValidationError
-# from lib.assertions import assert_valid_schema
-from jsonschema import validate
-from os.path import join, dirname
+from lib.assertions import assert_valid_schema
 import json
+import os
 
 class TestGetSliders():
     def test_schema_validation_for_sliders(self):
@@ -16,22 +15,9 @@ class TestGetSliders():
         #     print(e.json())
 
         # using jsonschema validate
-        def assert_valid_schema(data, schema_file):
-            """ Checks whether the given data matches the schema """
-
-            schema = _load_json_schema(schema_file)
-            return validate(data, schema)
-
-        def _load_json_schema(filename):
-            """ Loads the given schema file """
-
-            relative_path = join('schemas', filename)
-            absolute_path = join(dirname(__file__), relative_path)
-
-            with open(absolute_path) as schema_file:
-                return json.loads(schema_file.read())
-
         response = requests.get("https://api.dev.more.tv/app/Sliders/1")
         my_data = json.loads(response.text)
-        assert_valid_schema(my_data, "json_schema_for_first_slider.json")
+        my_json_path = r'D:\PythonAPI\LearnQA_PythonAPI\tests\schema_validation\schemas'
+        assert_valid_schema(my_data, "json_schema_for_first_slider.json", my_json_path)
+
 
